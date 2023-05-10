470a1f48b806fab8cfb71bd55f8ef6792cc142c6
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-05-10 10:14:40 +0100

    JS: functionality added

---
+++ main.js
@@ -0,0 +1,45 @@
+const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe')
+const gradients = document.querySelectorAll('.gradient')
+
+let PrevColor = "blue"
+
+animationEnd = true;
+
+function changeSize(){
+    sizes.forEach(size => size.classList.remove('active'))
+    this.classList.add('active')
+}
+
+function colorChange(){
+    if(!animationEnd) return;
+    let primary = this.getAttribute('primary')
+    let color = this.getAttribute('color')
+    let shoe = document.querySelector(`.shoe[color="${color}"]`)
+    let gradient = document.querySelector(`.gradient[color="${color}"]`)
+    let prevGrad = document.querySelector(`.gradient[color="${PrevColor}"]`)
+
+    colors.forEach(color => color.classList.remove('active'))
+    this.classList.add('active')
+
+    document.documentElement.style.setProperty('--primary', primary)
+    shoes.forEach(shoe => shoe.classList.remove('show'))
+    shoe.classList.add('show')
+
+    gradients.forEach(gradient => gradient.classList.remove('first', 'second'))
+    gradient.classList.add('first')
+    prevGrad.classList.add('second')
+
+    PrevColor = color;
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', ()=>{
+        animationEnd = true;
+    })
+
+
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize))
+colors.forEach(color => color.addEventListener('click', colorChange))
